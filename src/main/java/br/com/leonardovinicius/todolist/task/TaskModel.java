5ab4d3d584847de31d0d1340ef71c3f7c1431ea2
leonardo vinicius <leonardovinicius9987@gmail.com>
2024-11-20 17:13:13 -0300

    - Adicionar filtro para pegar informações do usuário logado. - Adicionar rotas de Post, Get e Put de tarefas

--- src/main/java/br/com/leonardovinicius/todolist/task/TaskModel.java
+++ src/main/java/br/com/leonardovinicius/todolist/task/TaskModel.java
@@ -26,12 +26,10 @@ public class TaskModel {
     private LocalDateTime startAt;
     private LocalDateTime endAt;
     private String priority;
+    private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-
-
-    private UUID idUser;
     
     
 }
